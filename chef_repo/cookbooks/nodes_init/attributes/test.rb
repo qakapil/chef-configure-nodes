3ed5c503ed8e3e8b23f334525c2bcea37649fc3c
ksharma <ksharma@suse.com>
2014-10-08 15:23:59 +0200

    removing lighthttp config

--- chef_repo/cookbooks/nodes_init/attributes/test.rb
+++
@@ -1,4 +0,0 @@
-default['nodes']['fqdn'] = ['teuthida-3.suse.de', 'teuthida-2.suse.de', 'teuthida-1.suse.de']
-default['nodes']['sname'] = ['teuthida-3', 'teuthida-2', 'teuthida-1']
-default['nodes']['master'] = ['teuthida-2', 'teuthida-1']
-default['nodes']['ip'] = ['10.120.223.3', '10.120.223.2', '10.120.223.1']
--- chef_repo/solo-root2.rb
+++
@@ -1,3 +0,0 @@
-file_cache_path "/tmp/chefroot2"
-cookbook_path "/root/workspace/chef_repo/cookbooks"
-
