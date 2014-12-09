cd79706e4a09bc57df727bc168636a9de6c5abd8
ksharma <ksharma@suse.com>
2014-12-09 19:52:20 +0100

    somehow the ifcfg- var was not valid, changed to ifcfg_

--- ipsettings.py
+++ ipsettings.py
@@ -13,25 +13,25 @@ grp2 = ['teuthida-8','teuthida-9','teuthida-10']
 document = open('nodes.yaml').read()
 ctx = yaml.load(document)
 fr = open("template", "r")
-ifcfg-template = fr.read()
+ifcfg_template = fr.read()
 fr.close()
 
 for node in ctx['config_nodes']:
     ip = socket.gethostbyname(node)
-    ifcfg-p1 = ifcfg-template % ('dhcp', ip)
-    private-ip = '10.0.0.'+ip.split(".")[3]
-    ifcfg-p2 = ifcfg-template % ('static', private-ip)
+    ifcfg_p1 = ifcfg_template % ('dhcp', ip)
+    private_ip = '10.0.0.'+ip.split(".")[3]
+    ifcfg_p2 = ifcfg_template % ('static', private_ip)
     if node in grp1:
-       nic-prefix = "p2"
+       nic_prefix = "p2"
     else:
-       nic-prefix = "p1"
-    filename1 = 'ifcfg-'+nic-prefix+'p1'
-    filename2 = 'ifcfg-'+nic-prefix+'p2'
+       nic_prefix = "p1"
+    filename1 = 'ifcfg_'+nic_prefix+'p1'
+    filename2 = 'ifcfg_'+nic_prefix+'p2'
     fw = open(filename1, "w")
-    fw.write(ifcfg-p1)
+    fw.write(ifcfg_p1)
     fw.close()
     fw = open(filename2, "w")
-    fw.write(ifcfg-p2)
+    fw.write(ifcfg_p2)
     fw.close()
 
     cmd = "scp %s %s root@%s:/etc/sysconfig/network/" % (filename1, filename2, node)
@@ -40,8 +40,8 @@ for node in ctx['config_nodes']:
         raise Exception, "Error while executing the command '%s'. \
                           Error message: '%s'" % (cmd, stderr)
 
-    interface1 = nic-prefix+'p1'
-    interface2 = nic-prefix+'p2'
+    interface1 = nic_prefix+'p1'
+    interface2 = nic_prefix+'p2'
     
     cmd = "ssh root@%s ifdown em1 && sleep 5 && ifup %s && sleep 10 && ifup %s && sleep 10 && route add default gw 10.160.255.254 %s"\
           % (node, interface1, interface2, interface1)
